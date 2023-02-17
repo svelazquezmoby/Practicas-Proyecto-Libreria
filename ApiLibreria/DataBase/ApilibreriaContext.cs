828b360c291dcfc8f35a0cbf1241e32d4eb07ea8
svelazquezmoby <svelazquez@mobydigital.com>
2023-02-17 14:32:23 -0300

    se cambio conexion bd

--- ApiLibreria/DataBase/ApilibreriaContext.cs
+++ ApiLibreria/DataBase/ApilibreriaContext.cs
@@ -26,7 +26,7 @@ public partial class ApilibreriaContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        base.OnConfiguring(optionsBuilder);
+        base.OnConfiguring(optionsBuilder);fd
     }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
