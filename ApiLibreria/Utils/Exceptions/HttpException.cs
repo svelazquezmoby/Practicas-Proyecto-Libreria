6afd350decf3bd188ea23e2c2c8fadce675715fb
svelazquezmoby <svelazquez@mobydigital.com>
2023-02-17 12:09:55 -0300

    nueva api

---
+++ ApiLibreria/Utils/Exceptions/HttpException.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+
+namespace Utils.Exceptions
+{
+    public class HttpException : Exception
+    {
+        public string Message { get; set; }
+        public HttpStatusCode ErrorCode { get; set; }
+        public HttpException(string message, HttpStatusCode errorCode = HttpStatusCode.InternalServerError)
+        {
+            Message = message;
+            ErrorCode = errorCode;
+        }
+    }
+}
---
+++ ApiLibreria/Utils/Filters/HttpExceptionFilter.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
+using Utils.Exceptions;
+
+namespace Utils.Filters
+{
+    public class HttpExceptionFilter : ExceptionFilterAttribute
+    {
+        public override void OnException(ExceptionContext context)
+        {
+            var error = new HttpException(
+                "Interval Server Error",
+                HttpStatusCode.InternalServerError);
+
+            context.HttpContext.Response.StatusCode = 500;
+
+            if (context.Exception is HttpException)
+            {
+                error = context.Exception as HttpException;
+                if (error != null)
+                {
+                    context.HttpContext.Response.StatusCode = (int)error.ErrorCode;
+                }
+            }
+
+            if (context.Exception.InnerException is HttpException)
+            {
+                error = context.Exception.InnerException as HttpException;
+                if (error != null)
+                {
+                    context.HttpContext.Response.StatusCode = (int)error.ErrorCode;
+                }
+            }
+
+            context.Result = new JsonResult(new { error = error != null ? error.Message : "Internal Server Error" });
+            base.OnException(context);
+        }
+    }
+}
