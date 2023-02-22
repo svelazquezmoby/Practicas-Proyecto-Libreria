6e96d087eefd85ede9c49c50000c79f22a5ac6a2
svelazquezmoby <svelazquez@mobydigital.com>
2023-02-22 09:37:43 -0300

    creacion de dtos

---
+++ ApiLibreria/DataBase/Dtos/Order/UpdateBookDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase.Dtos.Order
+{
+    public class UpdateBookDTO
+    {
+        public string Detail { get; set; } 
+
+        public DateTime DateStart { get; set; }
+
+        public DateTime DateFinish { get; set; }
+    }
+}
